d8a25ea7e86a1d128411f84446f2464b271ddbe8
kosuke mochihara <k__mochihara@hotmail.co.jp>
2023-08-12 21:14:04 +0900

    header等

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    //ナビバートグル
+    $('.js-hamburger').on('click', function () {
+      if ($('.js-hamburger').hasClass('is-open')) {
+        $('.js-drawer-menu').removeClass('is-open');
+        $(this).removeClass('is-open');
+      } else {
+        $('.js-drawer-menu').addClass('is-open');
+        $(this).addClass('is-open');
+      }
+    });
+  
+    $('.js-drawer-menu').on('click', function () {
+      $('.js-drawer-menu').removeClass('is-open');
+      $('.js-hamburger').removeClass('is-open');
+    });
 
 });
