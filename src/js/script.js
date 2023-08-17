638747c723aa882cba6a8e7e3fa49a8b76ef0404
kosuke mochihara <k__mochihara@hotmail.co.jp>
2023-08-18 08:09:31 +0900

    blog

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
     
 
-    const mySwiper = new Swiper('.js-mv .swiper', {
+    const SwiperMv = new Swiper('.js-mv .swiper', {
       slidesPerView: 'auto', // 何枚のカードを表示するか
       grabCursor: true, // マウスオーバー時にカーソルをグラブ状態にするか
       simulateTouch: false, // スワイプ操作をPCのブラウザでシミュレートするか
@@ -28,6 +28,31 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
         disableOnInteraction: false, // ユーザーのスワイプ操作後も自動再生を続行
       },
       speed: 3000, // 1秒（1000ミリ秒）かけてスライドが移動する
+    });
+
+    const mySwiper = new Swiper('.js-campaignSwiper .swiper', {
+      slidesPerView: 'auto', // 何枚のカードを表示するか
+      spaceBetween: 24, // カードの間隔
+      grabCursor: true, // マウスオーバー時にカーソルをグラブ状態にするか
+      loop: true, // ループさせるかどうか
+        autoplay: {
+        delay: 2500, // 2.5秒ごとにスライド
+        disableOnInteraction: false, // ユーザーのスワイプ操作後も自動再生を続行
+      },
+      speed: 2000, // 1秒（1000ミリ秒）かけてスライドが移動する
+      pagination: {
+        el: '.js-campaignSwiper .swiper-pagination', // ページネーションの要素
+        clickable: true, // ページネーションをクリックできるようにするか
+      },
+      navigation: {
+        nextEl: '.js-campaignSwiper .swiper-button-prev', // 次のボタン要素
+        prevEl: '.js-campaignSwiper .swiper-button-next', // 前のボタン要素
+      },
+      breakpoints: { // 画面幅によってオプションを変える
+        768: {
+          spaceBetween: 40,   // カードの間隔
+        }
+      },
     });
 
 });
